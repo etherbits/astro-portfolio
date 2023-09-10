e3e8f136ee3bae76cd36482711b150e47120b4a5
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-10 18:25:53 +0400

    adding the button component

--- src/utils/tailwind.ts
+++ src/utils/tailwind.ts
@@ -4,3 +4,10 @@ import { type ClassValue, clsx } from "clsx";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function prefixClass(prefix: string, className: string) {
+  return className
+    .split(" ")
+    .map((cn) => prefix + cn)
+    .join(" ");
+}

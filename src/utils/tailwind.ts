a0f9f941c844bd5efcf92f46e044c39cf49deb8c
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-10 10:54:56 +0400

    migrating desktop nav

--- src/utils/tailwind.ts
+++ src/utils/tailwind.ts
@@ -1,37 +1,6 @@
 import { twMerge } from "tailwind-merge";
 import { type ClassValue, clsx } from "clsx";
-import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "../../tailwind.config.cjs";
-
-const tailwindConf = resolveConfig(tailwindConfig);
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export function getColor(color: string) {
-  const [label, shade] = color.split("-");
-  const colors = tailwindConf.theme?.colors;
-
-  if (!colors) return null;
-
-  const twColor = colors[label];
-
-  if (typeof twColor === "string") return twColor;
-  return twColor[shade] as string;
-}
-
-export function getColorRGBA(color: string, alpha = 1) {
-  const twColor = getColor(color);
-
-  if (!twColor) return "";
-
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(twColor);
-
-  return result
-    ? `rgba(${parseInt(result[1], 16)},${parseInt(result[2], 16)},${parseInt(
-        result[3],
-        16,
-      )},${alpha})`
-    : "";
 }

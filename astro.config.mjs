3c0a56c07f682c663ab820b50326a09ed22df65c
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-11 23:28:55 +0400

    adding redirects for locale

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  redirects: {
+    "/projects": "/en/projects",
+    "/journey": "/en/journey",
+    "/contact-me": "/en/contact-me",
+  },
 });

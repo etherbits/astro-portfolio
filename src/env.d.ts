b244074715b39fd1f2810c37707c841a07a2bf59
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-12 23:07:06 +0400

    burger drop animation using anime.js

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 declare namespace App {
-    interface Locals {
-      locale: string
-    }
+  interface Locals {
+    locale: string;
+  }
 }

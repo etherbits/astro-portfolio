562da8501c1e195b5e4e0389fbeebe21393500dc
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-12 17:03:44 +0400

    writing middleware for locales

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -3,11 +3,11 @@ import { ui, defaultLang, type TranslationKey } from "./ui";
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split("/");
   if (lang in ui) return lang as keyof typeof ui;
-  return defaultLang;
+  return null;
 }
 
 export function generateTranslator(lang: keyof typeof ui) {
   return function t(key: TranslationKey) {
-    return ui[lang][key] || ui[defaultLang][key];
+    return ui[lang]?.[key] || ui[defaultLang][key];
   };
 }

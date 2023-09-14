fa4a79c6469ee02398fe7fb45e11d89d6a295575
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-14 12:42:16 +0400

    updating project collection schema to add missing fields

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,9 +5,11 @@ const projectCollection = defineCollection({
   schema: z.object({
     title: z.string().min(1).max(128),
     description: z.string().min(1).max(1000),
+    image: z.string().min(1),
     repoLink: z.string().url(),
     websiteLink: z.string().url().optional(),
     demoLink: z.string().url().optional(),
+    disclamer: z.string().min(1),
     tags: z.array(z.string().min(1).max(32)).min(5).max(10),
   }),
 });
--- src/content/projects/ka/unispace.md
+++ src/content/projects/ka/unispace.md
@@ -1,7 +1,8 @@
 ---
 title: "UniSpace"
 description: "ქართულია :დ"
+image: "/images/unispace-laptop.webp"
 repoLink: "https://github.com/etherbits/Unilab-Internal-System"
-tags:
-  ["sometihng", "something", "something", "sometihng", "something", "something"]
+disclamer: "* Does not support mobile for now"
+tags: ["sometihng", "something", "something", "sometihng", "something", "something"]
 ---

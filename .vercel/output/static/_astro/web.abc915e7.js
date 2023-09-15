0c4d800a63e53787589d57d4a868c68b875ccf27
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-15 18:00:19 +0400

    removing extra files

--- .vercel/output/static/_astro/web.abc915e7.js
+++
@@ -1,8 +0,0 @@
-const e="en/web.md",n="journeys",t="en/web",o="",r={title:"Surge of Expertise",content:"During 2019-2020, my journey evolved. Leveraging the vast knowledge on the internet, I honed my focus on web development, propelling my progress. Amidst the pandemic, I embarked on a significant project – crafting a class management website for my mother's work. This endeavor propelled me even further.",date:"2019"},s={type:"content",filePath:"/home/etherbits/projects/astro-portfolio/src/content/journeys/en/web.md",rawData:`
-title: Surge of Expertise
-content:
-  During 2019-2020, my journey evolved. Leveraging the vast knowledge on the internet,
-  I honed my focus on web development, propelling my progress. Amidst the pandemic,
-  I embarked on a significant project – crafting a class management website for my mother's work.
-  This endeavor propelled me even further.
-date: "2019"`};export{s as _internal,o as body,n as collection,r as data,e as id,t as slug};

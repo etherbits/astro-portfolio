06a9f3b4a0c403ed91c61c46f6246c12397158ec
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-17 18:06:04 +0400

    adding SEO descriptions

---
+++ src/i18n/seo.ts
@@ -0,0 +1,8 @@
+export const seo = {
+  en: {
+    "description": "Portfolio of Nika Qvrivishvili (Nikoloz Kvrivishvili), a Web Developer from Georgia, who creates programs and software with the use of web technologies."
+  },
+  ka: {
+    "description": "ვებ დეველოპერის, ნიკა ქვრივიშვილის (ნიკოლოზ ქვრივიშვილის), პორტფოლიო. პროგრამისტი, რომელიც ქმნის პროგრამებს ქსელური ტექნოლოგიების გამოყენებით."
+  }
+}

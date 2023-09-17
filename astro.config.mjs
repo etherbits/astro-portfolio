d9fa00401f7b67fa0e929787f802e9f85efb60ca
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-17 20:39:04 +0400

    switching to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-// import {loadEnv} from 'vite'
-
-// const {Sec}
-
 import prefetch from "@astrojs/prefetch";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   site: "https://nikaa.online",
   integrations: [tailwind(), prefetch(), sitemap()],
   adapter: vercel()
